95427145fd333e9500b3a35d99ee458933258e7a
Jerald1904 <albertjerald007@gmail.com>
2025-10-09 17:31:23 +0000

    auto- trigger is successful, final output is updated

--- app/app.py
+++ app/app.py
@@ -8,20 +8,14 @@ def home():
     return (
         "This is a sample Flask application.<br>"
         "I am Albert Jerald — thank you for visiting!<br><br>"
-        "This app is part of a simple CI/CD pipeline demo "
-    )
-
-def main():
-    print("=== Welcome to Albert Jerald’s Flask App ===\n")
-    paragraph = (
-        "I'm Albert Jerald "
-        "I'm open for new opportunities. I'm practicing AWS and Devops tools "
-        "I have hands-on experience on AWS, Linux, Git, Jenkins, Docker and practical knowledge on Ansible and Terraform. "
-        "Contact my Email: albertjerald19@gmail.com  "
+        "This app is part of a simple CI/CD pipeline demo <br><br><br>"
+        "Hi, I'm Albert Jerald <br><br>"
+        "I'm practicing AWS and Devops tools <br>"
+        "I have used AWS EC2, Linux, Git, Jenkins, Docker for this demo.<br> "
+	"I'm planning to use Ansible, Terraform and Kubernetes in upcoming project.<br>"
+        "Contact my Email: albertjerald19@gmail.com  <br>"
         "Contact my Mobile Number : 8939263846/ 8072545136"
     )
-    print(paragraph)
-
 if __name__ == "__main__":
     main()
     app.run(host="0.0.0.0", port=5000)
